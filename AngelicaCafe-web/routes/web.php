eb1a17caa3514cd6d8fb4296542d7955e299c6b4
BagusGilangRachmadi <bagusgilangrachmadi.1@gmail.com>
2024-04-02 12:18:13 +0700

    commit

--- AngelicaCafe-web/routes/web.php
+++ AngelicaCafe-web/routes/web.php
@@ -13,14 +13,3 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/about', function () {
-    return view('about');
-});
-
-
-    Route::get('produks', [ProdukController::class, 'index']);
-    Route::get('produks/create', [ProdukController::class, 'create'])->name('admin.produks.create');;
-    Route::post('produks/store', [ProdukController::class, 'store'])->name('admin.produks.store');
-    Route::get('produks/update/{id}', [ProdukController::class, 'edit']);
-    Route::put('produks/update/{id}', [ProdukController::class, 'update'])->name('admin.produks.update');
-    Route::delete('produks/delete/{id}',[ProdukController::class, 'delete'])->name('admin.produks.delete');
