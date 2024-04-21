8bc6fdf9f33a37c465a1a9c9e573f8fb60926a7a
mysfarkan <mysfarkan@gmail.com>
2024-04-21 12:09:00 +0700

    KEL406-20 update route pada fitur order admin

--- AngelicaCafe-web/routes/web.php
+++ AngelicaCafe-web/routes/web.php
@@ -60,15 +60,15 @@ Route::post('/login',[AuthController::class, 'login'])->name('login');
 Route::post('/signup',[AuthController::class, 'register'])->name('register');
 Route::get('/cekUser',[AuthController::class, 'cekUser'])->name('cekuser');
 
-Route::get('orders',[OrderController::class,'index'])->name('admin.orders.indes');
-Route::get('orders/detail/{id}',[OrderController::class,'detailOrder'])->name('admin.orders.detail');
-Route::put('orders/status/update/{id}',[OrderController::class,'updateStatusOrder'])->name('admin.orders.update.status');
 Route::get('/review',[ReviewController::class,'index']);
 Route::post('/review/store',[ReviewController::class,'store'])->name('review.store');
 
 Route::prefix('admin')->group(function () {
     Route::get('/aboutUs/edit', [AdminAboutUsController::class, 'edit'])->name('admin.aboutUs.edit');
     Route::post('/aboutUs/update', [AdminAboutUsController::class, 'update'])->name('admin.aboutUs.update');
+    Route::get('orders',[OrderController::class,'index'])->name('admin.orders.indes');
+    Route::get('orders/detail/{id}',[OrderController::class,'detailOrder'])->name('admin.orders.detail');
+    Route::put('orders/status/update/{id}',[OrderController::class,'updateStatusOrder'])->name('admin.orders.update.status');
 });
 
 Route::get('/about', [AboutusController::class, 'index'])->name('aboutus');
