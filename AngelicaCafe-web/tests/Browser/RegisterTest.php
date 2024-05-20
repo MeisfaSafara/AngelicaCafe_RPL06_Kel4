dc4154ca5c8a93063cbb801b59a6dd0738f8bb4c
KyandraK <kyandra.kusuma@gmail.com>
2024-05-20 14:32:18 +0700

    Automated testing

---
+++ AngelicaCafe-web/tests/Browser/RegisterTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Tests\Browser;
+
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Laravel\Dusk\Browser;
+use Tests\DuskTestCase;
+
+class RegisterTest extends DuskTestCase
+{
+    /**
+     * A Dusk test example.
+     * @group register
+     */
+    public function testExample(): void
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/')
+                    ->clickLink('Menu')
+                    ->assertPathIs('/menu');
+        });
+    }
+}
