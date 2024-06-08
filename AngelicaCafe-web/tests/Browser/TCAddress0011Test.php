3c3c79a88d7df638773cbbe9dc2cf0e4463eb445
ramaptr <ramaputrar213@gmail.com>
2024-06-08 15:34:09 +0700

    testing address

---
+++ AngelicaCafe-web/tests/Browser/TCAddress0011Test.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Tests\Browser;
+
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Laravel\Dusk\Browser;
+use Tests\DuskTestCase;
+
+class TCAddress0011Test extends DuskTestCase
+{
+    /**
+     * A Dusk test example.
+     * @group TCAddress0011Test
+     */
+    public function testExample(): void
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/login')
+                    ->type('email', 'user@gmail.com')
+                    ->type('password', 'user@gmail.com') 
+                    ->press('Login') 
+                    ->assertPathIs('/profile')
+                    ->assertSee('Address List')
+                    ->clickLink('Address List')
+                    ->assertPathIs('/profile/address')
+                    ->assertSee('Add New Address')
+                    ->clickLink('Add New Address')
+                    ->assertPathIs('/profile/address/add')
+                    ->type('street', 'Sukabirus')
+                    ->type('city', 'Metropolis')
+                    ->type('state', 'Canada')
+                    ->type('postal_code', '12345')
+                    ->press('Save')
+                    ->assertPathIs('/profile/address')
+            ;
+        });
+    }
+}
